8fb87b3d1890eec6ee44e9193fac15ab533be55a
dair68 <granthuang68@yahoo.com>
2023-10-02 16:48:20 -0700

    dynamic amount of hint inputs in form

--- logic/create.js
+++ logic/create.js
@@ -20,10 +20,30 @@ const solutionEditor = new Quill("#solution-editor", {
     placeholder: "It's over 9000!"
 });
 
-const hintEditor = new Quill("#hint-1-editor", {
-    modules: {
-        toolbar: toolbarOptions
-    },
-    theme: "snow",
-    placeholder: "First Hint"
-});
+const hintEditors = [];
+
+//creating editors for each hint input
+for (let i = 1; i <= 4; i++) {
+    const editor = new Quill("#hint-" + i, {
+        modules: {
+            toolbar: toolbarOptions
+        },
+        theme: "snow",
+        placeholder: "Input hint here"
+    });
+    
+    hintEditors.push(editor);
+}
+
+//adjusts number of hint inputs in form based on "Number of hints" radio buttons
+//@param radioButton - radio button that was just selected
+function adjustHints(radioButton) {
+    const hints = radioButton.value;
+    console.log(hints);
+
+    //showing and hiding hint inputs as needed
+    for (let i = 1; i <= 4; i++) {
+        const editor = document.getElementById("hint-container-" + i);
+        editor.style["display"] = (i <= hints) ? "block" : "none";
+    }
+}
